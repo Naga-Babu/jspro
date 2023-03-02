750bed5241f3a9e269524db3109035f056956c8f
Naga-Babu <nagababugade6294@gmail.com>
2023-03-02 13:00:46 +0530

    initial commit to git

---
+++ arthametic.js
@@ -0,0 +1,7 @@
+
+const currentYear = 2023;
+const dob = 1998;
+const ageNagababu=currentYear-dob;
+const ageMadhavi=currentYear-dob;
+// const ageMadhavi=now-1998;
+console.log(ageMadhavi,ageNagababu);
